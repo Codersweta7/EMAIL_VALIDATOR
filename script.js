dc35862f953471fb9e414b3cd1909c4dffcad43c
Codersweta7 <sweta803pandey@gmail.com>
2024-11-29 20:15:56 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+console.log("hii");
 let result=
     {
     "tag": "",
@@ -17,8 +19,24 @@ let result=
     "format_valid": true
   }
 
+
+submitbtn.addEventListener("click",async()=>{
+    console.log("scriptstarts");
+    let key="ema_live_7w3BCwzNcnAy7bRwnl7OJTuCqOxxenvaVVLxVSmw"
+    let email=document.getElementById("emails").value
+
+let url=`https://api.emailvalidation.io/v1/info?apikey=${key}email={email}`
+let resp= await fetch(url)
+let result=await resp.json()
+
+
 let str=''
 for (key of Object.keys(result)){
  str=str+`<div>${key}:${result[key]}</div>`
 }
 resultCont.innerHTML=str;
+})
+
+
+
+
